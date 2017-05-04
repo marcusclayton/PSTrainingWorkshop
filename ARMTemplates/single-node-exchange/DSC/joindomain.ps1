8ff050336e774364efe3f14c9e797000dcaefd7b
Marcus <marcusaclayton@gmail.com>
2017-05-03 18:41:42 -0700

    initial commit

---
+++ ARMTemplates/single-node-exchange/DSC/joindomain.ps1
@@ -0,0 +1,48 @@
+Configuration JoinDomain
+{
+       param ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+
+    )
+
+    Import-DscResource -ModuleName xDSCDomainjoin,xStorage,PSDesiredStateConfiguration
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk {
+            DiskNumber = 2
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
+
+        xDSCDomainjoin JoinDomain
+        {
+            Domain = $DomainName
+            Credential = $Admincreds
+            DependsOn = @("[xDisk]ADDataDisk")
+        }
+    }
+}
